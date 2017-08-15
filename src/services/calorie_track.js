6239c44dadfec6cdbf046d8453c4fbda2740cfbc
Endre Vegh <endre.vegh@yahoo.com>
2017-08-15 12:59:32 +0100

    nutr

--- src/services/calorie_track.js
+++ src/services/calorie_track.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as _ from 'lodash';
 import { fromJS } from 'immutable';
-import config from './../../config/config.json';
+// import config from './../../config/config.json';
 
 // const {
 //   dev: { nutritionix: { API_ENDPOINT, REF_KEY, APP_KEY, APP_ID } }
